91035699a1367b9c946430c9e19e1d007d74f8cf
Guille <glexposito@gmail.com>
2025-05-17 22:09:06 +1200

    Remove unnecessary blank line in Program.cs

--- Greetings.Api/Program.cs
+++ Greetings.Api/Program.cs
@@ -11,7 +11,6 @@ var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 });
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
